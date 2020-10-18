4d3faec9d5fb36b87a86a4be4fa0d5459839825b
Brino Droid <tdview80@gmail.com>
2020-10-17 20:25:47 -0700

    Adding bootstrap to app

--- frontend/fe/src/App.js
+++ frontend/fe/src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="#home">
+            Brino
+        </Navbar.Brand>
+      </Navbar>
     </div>
   );
 }
