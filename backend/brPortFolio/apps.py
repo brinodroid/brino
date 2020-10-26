1c882dd1f669883e505d5fb45972f80524834685
Brino Droid <tdview80@gmail.com>
2020-10-26 00:10:32 -0700

    Removing brPortFolio and brWatchlist

--- backend/brPortFolio/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BrportfolioConfig(AppConfig):
-    name = 'brPortFolio'
--- backend/brWatchList/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BrwatchlistConfig(AppConfig):
-    name = 'brWatchList'
