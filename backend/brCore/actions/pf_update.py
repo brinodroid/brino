50e710ee6c9bcc65567c212f3c8c8ee17060bbbf
Brino Droid <tdview80@gmail.com>
2021-01-10 12:41:03 -0800

    Update watchlist, portfolio and scan for stocks too

---
+++ backend/brCore/actions/pf_update.py
@@ -0,0 +1,218 @@
+import logging
+
+from brCore.client.Factory import get_client
+from brCore.models import WatchList, BGTask, PortFolio, ScanEntry, PortFolioUpdate
+from brSetting.models import Configuration
+from brCore.types.asset_types import PortFolioSource, AssetTypes, TransactionType
+from brCore.types.scan_types import ScanProfile
+
+logger = logging.getLogger('django')
+
+
+class PFUpdater:
+    __instance = None
+
+    __scan_profile_for_call_option_lut = {
+        TransactionType.BUY.value: ScanProfile.BUY_CALL.value,
+        TransactionType.SELL.value: ScanProfile.SELL_CALL.value,
+    }
+
+    __scan_profile_for_put_option_lut = {
+        TransactionType.BUY.value: ScanProfile.BUY_PUT.value,
+        TransactionType.SELL.value: ScanProfile.SELL_PUT.value,
+    }
+
+    def __init__(self):
+        """ Virtually private constructor. """
+        if PFUpdater.__instance != None:
+            raise Exception("This class is a singleton!")
+        else:
+            PFUpdater.__instance = self
+
+    @staticmethod
+    def getInstance():
+        """ Static access method. """
+        if PFUpdater.__instance == None:
+            PFUpdater()
+        return PFUpdater.__instance
+
+    def __update_option_in_watchlist(self, option_data):
+        try:
+            watchlist = WatchList.objects.get(asset_type=option_data['brino_asset_type'],
+                                              ticker=option_data['chain_symbol'],
+                                              option_strike=option_data['strike_price'],
+                                              option_expiry=option_data['expiration_date'])
+            # Watchlist already have the entry, just update the brine_id
+            watchlist.brine_id = option_data['id']
+        except WatchList.DoesNotExist:
+            # Create a new watchlist
+            watchlist = WatchList(asset_type=option_data['brino_asset_type'],
+                                  ticker=option_data['chain_symbol'],
+                                  option_strike=option_data['strike_price'],
+                                  option_expiry=option_data['expiration_date'],
+                                  brine_id=option_data['id'])
+        watchlist.save()
+        return watchlist
+
+    def __update_option_in_portfolio(self, option, watchlist, configuration):
+        # Check if its already there in portfolio
+        try:
+            portfolio = PortFolio.objects.get(brine_id=option['id'])
+            # Portfolio already has the entry. Nothing to do
+            return portfolio
+        except PortFolio.DoesNotExist:
+            logger.info('__update_option_in_portfolio: Adding watchlist_id to portfolio'.format(
+                watchlist.id))
+            # INTENTIONAL FALL DOWN. Add the entry to portfolio
+
+        # Portfolio has to be identified by the brine_id. Add it the right brine id
+        portfolio = PortFolio(watchlist_id=watchlist.id,
+                              entry_datetime=option['created_at'],
+                              entry_price=option['brino_entry_price'],
+                              units=float(
+                                  option['quantity'])*float(option['trade_value_multiplier']),
+                              transaction_type=option['brino_transaction_type'],
+                              brine_id=option['id'],
+                              source=PortFolioSource.BRINE.value)
+        portfolio.save()
+        return portfolio
+
+    def __get_scan_profile(self, watchlist, portfolio):
+        if watchlist.asset_type == AssetTypes.STOCK.value:
+            return ScanProfile.BUY_STOCK.value
+
+        if watchlist.asset_type == AssetTypes.PUT_OPTION.value:
+            return self.__scan_profile_for_put_option_lut[portfolio.transaction_type]
+
+        # Else its call option
+        return self.__scan_profile_for_call_option_lut[portfolio.transaction_type]
+
+    def __update_scan_entry(self, watchlist, portfolio, configuration):
+        profile = self.__get_scan_profile(watchlist, portfolio)
+
+        # Check if its already there in scan
+        try:
+            scan_entry = ScanEntry.objects.get(
+                watchlist_id=watchlist.id, profile=profile)
+            # Portfolio already has the entry. Nothing to do
+            return scan_entry
+        except ScanEntry.DoesNotExist:
+            logger.info('__update_option_in_scan_entry: Adding watchlist_id={}, profile={} to ScanEntry'
+                        .format(watchlist.id, profile))
+            # Add the entry to ScanEntry
+
+        # ScanEntry has to be identified by the watchlist_id and profile. Adding new scan entry
+
+        if portfolio.transaction_type == TransactionType.BUY.value:
+            # When we buy,
+            # 1. profit target should be more than entry_price
+            # 2. stop loss should be less than entry_price
+            profit_target = portfolio.entry_price * \
+                (100+configuration.profitTargetPercent)/100
+            stop_loss = portfolio.entry_price * \
+                (100-configuration.stopLossPercent)/100
+        else:
+            # When we sell,
+            # 1. profit target should be less than entry_price
+            # 2. stop loss should be more than entry_price
+            profit_target = portfolio.entry_price * \
+                (100-configuration.profitTargetPercent)/100
+            stop_loss = portfolio.entry_price * \
+                (100+configuration.stopLossPercent)/100
+
+        scan_entry = ScanEntry(watchlist_id=watchlist.id,
+                               profile=profile,
+                               profit_target=profit_target,
+                               stop_loss=stop_loss)
+        scan_entry.save()
+        return scan_entry
+
+    def __update_options(self, client, configuration):
+        option_list = client.get_my_options_list()
+
+        for option in option_list:
+            # Check if we already have this in watchlist
+            try:
+                watchlist = WatchList.objects.get(
+                    brine_id=option['option_id'])
+            except WatchList.DoesNotExist:
+                # Call client to get more info
+                option_data = client.get_option_data(option['option_id'])
+                watchlist = self.__update_option_in_watchlist(option_data)
+
+            # Update portfolio
+            portfolio = self.__update_option_in_portfolio(
+                option, watchlist, configuration)
+
+            self.__update_scan_entry(watchlist, portfolio, configuration)
+        return
+
+    def __update_stock_in_watchlist(self, stock, instrument_data):
+        try:
+            watchlist = WatchList.objects.get(asset_type=stock['brino_asset_type'],
+                                              ticker=instrument_data['symbol'])
+            # Watchlist already have the entry, just update the brine_id
+            watchlist.brine_id = stock['instrument_id']
+        except WatchList.DoesNotExist:
+            # Create a new watchlist
+            watchlist = WatchList(asset_type=stock['brino_asset_type'],
+                                  ticker=instrument_data['symbol'],
+                                  brine_id=stock['instrument_id'])
+        watchlist.save()
+        return watchlist
+
+    def __update_stock_in_portfolio(self, watchlist, stock):
+        # Check if its already there in portfolio
+        try:
+            portfolio = PortFolio.objects.get(brine_id=stock['instrument_id'])
+            # Portfolio already has the entry. Nothing to do
+            return portfolio
+        except PortFolio.DoesNotExist:
+            logger.info('__update_stock_in_portfolio: Adding watchlist_id to portfolio'.format(
+                watchlist.id))
+            # INTENTIONAL FALL DOWN. Add the entry to portfolio
+
+        # Portfolio has to be identified by the brine_id. Add it the right brine id
+        portfolio = PortFolio(watchlist_id=watchlist.id,
+                              entry_datetime=stock['created_at'],
+                              entry_price=stock['brino_entry_price'],
+                              units=float(stock['quantity']),
+                              transaction_type=stock['brino_transaction_type'],
+                              brine_id=stock['instrument_id'],
+                              source=PortFolioSource.BRINE.value)
+        portfolio.save()
+        return portfolio
+
+    def __update_stocks(self, client, configuration):
+        stock_list = client.get_my_stock_list()
+
+        for stock in stock_list:
+            # Update watchlist
+            try:
+                watchlist = WatchList.objects.get(
+                    brine_id=stock['instrument_id'])
+            except WatchList.DoesNotExist:
+                instrument_data = client.get_stock_instrument_data(
+                    stock['instrument_url'])
+                watchlist = self.__update_stock_in_watchlist(
+                    stock, instrument_data)
+
+            # Update portfolio
+            portfolio = self.__update_stock_in_portfolio(watchlist, stock)
+
+            # Update the scan entry
+            self.__update_scan_entry(watchlist, portfolio, configuration)
+
+        return
+
+    def update(self, source):
+        # TODO:
+        # 1. Need to handle other sources
+        # 2. Should we do something for delete?
+        # 3. Need to add for stocks too
+
+        configuration = Configuration.objects.first()
+        client = get_client(source)
+
+        self.__update_stocks(client, configuration)
+        self.__update_options(client, configuration)
--- backend/brCore/client/brine_adapter.py
+++ backend/brCore/client/brine_adapter.py
@@ -1,3 +1,4 @@
+import re
 import brine
 from brCore.types.asset_types import PortFolioSource, AssetTypes, TransactionType
 
@@ -33,10 +34,12 @@ class BrineAdapter:
             res_option['option_id'] = option['option_id']
             res_option['brino_transaction_type'] = self.__transaction_type_lut[option['type']]
             if res_option['brino_transaction_type'] == TransactionType.BUY.value:
-                res_option['brino_entry_price'] = float(res_option['average_price']);
+                res_option['brino_entry_price'] = float(
+                    res_option['average_price'])
             else:
-                #Gives negative value for price for selling transactions. Make it positve
-                res_option['brino_entry_price'] = -float(res_option['average_price']);
+                # Gives negative value for price for selling transactions. Make it positve
+                res_option['brino_entry_price'] = - \
+                    float(res_option['average_price'])
 
             res_option_list.append(res_option)
 
@@ -47,3 +50,31 @@ class BrineAdapter:
         # Modify the asset type
         option_data['brino_asset_type'] = self.__option_data_type_lut[option_data['type']]
         return option_data
+
+    def get_my_stock_list(self):
+        res_stock_list = []
+        stock_list = brine.account.get_open_stock_positions()
+        for stock in stock_list:
+            res_stock = {}
+            res_stock['client'] = PortFolioSource.BRINE.value
+            res_stock['brino_asset_type'] = AssetTypes.STOCK.value
+            res_stock['brino_transaction_type'] = TransactionType.BUY.value
+
+            res_stock['brino_entry_price'] = float(stock['average_buy_price'])
+            res_stock['quantity'] = stock['quantity']
+            res_stock['shares_available_for_exercise'] = stock['shares_available_for_exercise']
+            res_stock['shares_held_for_options_collateral'] = stock['shares_held_for_options_collateral']
+            res_stock['instrument_url'] = stock['instrument']
+            res_stock['created_at'] = stock['created_at']
+
+            # Extract the instrument id from the url
+            res_stock['instrument_id'] = re.search(
+                'https://api.robinhood.com/instruments/(.+?)/', stock['instrument']).group(1)
+
+            res_stock_list.append(res_stock)
+
+        return res_stock_list
+
+    def get_stock_instrument_data(self, instrument_url):
+        stock_data = brine.stocks.get_instrument_by_url(instrument_url)
+        return stock_data
