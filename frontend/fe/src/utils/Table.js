d0652a92bc82fd73de97215f2ac5f8b6d335dace
Brino Droid <tdview80@gmail.com>
2021-01-17 17:19:28 -0800

    Adding filtering in tables

--- frontend/fe/src/utils/Table.js
+++ frontend/fe/src/utils/Table.js
@@ -1,24 +1,100 @@
 import React from 'react';
-import { useTable } from 'react-table';
+import { useTable, useFilters } from 'react-table';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import BTable from 'react-bootstrap/Table';
+import FormControl from 'react-bootstrap/FormControl';
+import InputGroup from 'react-bootstrap/InputGroup';
+
+import {matchSorter} from 'match-sorter'
+
+
+// Define a default UI for filtering
+function DefaultColumnFilter({
+  column: { filterValue, preFilteredRows, setFilter },
+}) {
+  const count = preFilteredRows.length
+
+  return (
+    <InputGroup className="mb-3">
+    <FormControl
+      placeholder={`Search ${count} records...`}
+      aria-label="Amount (toasdf the nearest dollar)"
+      value={filterValue || ''}
+      onChange={e => {
+        setFilter(e.target.value || undefined) // Set undefined to remove the filter entirely
+      }}
+    />
+  </InputGroup>
+  )
+}
+
+function fuzzyTextFilterFn(rows, id, filterValue) {
+  return matchSorter(rows, filterValue, { keys: [row => row.values[id]] })
+}
 
 function Table({ columns, data }) {
+
+  const filterTypes = React.useMemo(
+    () => ({
+      // Add a new fuzzyTextFilterFn filter type.
+      fuzzyText: fuzzyTextFilterFn,
+      // Or, override the default text filter to use
+      // "startWith"
+      text: (rows, id, filterValue) => {
+        return rows.filter(row => {
+          const rowValue = row.values[id]
+          return rowValue !== undefined
+            ? String(rowValue)
+                .toLowerCase()
+                .startsWith(String(filterValue).toLowerCase())
+            : true
+        })
+      },
+    }),
+    []
+  )
+
+  const defaultColumn = React.useMemo(
+    () => ({
+      // Let's set up our default Filter UI
+      Filter: DefaultColumnFilter,
+    }),
+    []
+  )
+
   // Use the state and functions returned from useTable to build your UI
-  const { getTableProps, headerGroups, rows, prepareRow } = useTable({
-    columns,
-    data,
-  })
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+    state,
+    visibleColumns,
+    preGlobalFilteredRows,
+    setGlobalFilter,
+  } = useTable(
+    {
+      columns,
+      data,
+      defaultColumn, // Be sure to pass the defaultColumn option
+      filterTypes,
+    },
+    useFilters, // useFilters!
+  )
 
   // Render the UI for your table
   return (
-    <BTable striped bordered hover size="sm" {...getTableProps()}>
+    <BTable striped bordered hover size="sm" variant="dark" {...getTableProps()}>
       <thead>
         {headerGroups.map(headerGroup => (
           <tr {...headerGroup.getHeaderGroupProps()}>
             {headerGroup.headers.map(column => (
               <th {...column.getHeaderProps()}>
                 {column.render('Header')}
+                {/* Render the columns filter UI */}
+                <div>{column.canFilter ? column.render('Filter') : null}</div>
+
               </th>
             ))}
           </tr>
@@ -43,6 +119,8 @@ function Table({ columns, data }) {
     </BTable>
   )
 }
+
+
 
 export default Table;
 
