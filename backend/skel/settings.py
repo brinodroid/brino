662c6bebf29eae560241b630681ff4e1cd948bdd
Brino Droid <tdview80@gmail.com>
2022-04-20 00:52:23 -0700

    Switch to mysql

--- backend/skel/settings.py
+++ backend/skel/settings.py
@@ -89,10 +89,21 @@ WSGI_APPLICATION = 'skel.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['DATABASE_NAME'],
+        'USER': os.environ['DATABASE_USER'],
+        'PASSWORD': os.environ['DATABASE_PASSWORD'],
+        'HOST': '',
+        'PORT': '',
     }
 }
 
